8f3d1cf409cbf577bb4c2a9c45d5cb89293128b3
CHANDANA N KUMAR <80220098+chandanankumar@users.noreply.github.com>
2021-08-20 11:07:16 +0530

    Add files via upload

--- database/bank.sql
+++ database/bank.sql
@@ -1,1 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 20, 2021 at 07:35 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transfer`
+--
+
+CREATE TABLE `transfer` (
+  `sid` int(255) NOT NULL,
+  `sender` varchar(255) NOT NULL,
+  `receiver` varchar(255) NOT NULL,
+  `amount` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transfer`
+--
+
+INSERT INTO `transfer` (`sid`, `sender`, `receiver`, `amount`) VALUES
+(1, 'Anika Singh', 'Esha kumar', 1000),
+(2, 'Bindu N', 'Yukt B', 500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `balance` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
+(1, 'Anika Singh', 'anika@gmail.com', 9000),
+(2, 'Bindu N', 'bindu@gmail.com', 49500),
+(3, 'Charu Mehra', 'charu@gmail.com', 500),
+(4, 'Tarun T', 'tarun@gmail.com', 1000),
+(5, 'Esha kumar', 'kumar@gmail.com', 1100),
+(6, 'Mehul B', 'meh@gmail.com', 256),
+(7, 'Jiya Noon', 'jiya@gmail.com', 5020),
+(8, 'Karan Mehta', 'karan@gmail.com', 7045),
+(9, 'Yukt B', 'yukt@gmail.com', 650),
+(10, 'Ram Tapar', 'ram@gmail.com', 7500),
+(11, 'Hani', 'hani@gmail.com', 500);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transfer`
+--
+ALTER TABLE `transfer`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transfer`
+--
+ALTER TABLE `transfer`
+  MODIFY `sid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
