93341355771bc99b91a7aea432cabf12ed180962
CHANDANA N KUMAR <80220098+chandanankumar@users.noreply.github.com>
2021-08-20 10:48:48 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,3 @@
+<footer class="text-center" style="background-color:#000033; color: #f5deb3;">
+<p>&copy 2021. Made by <b>Chandana N</b> <br> For the Project of  <b>The Sparks Foundation</b></p>
+</footer>
