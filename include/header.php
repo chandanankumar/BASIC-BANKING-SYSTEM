c57c2d301a2e38f6f996e66bd6a2b4aecf845c7d
CHANDANA N KUMAR <80220098+chandanankumar@users.noreply.github.com>
2021-08-20 11:00:53 +0530

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-fixed-top " style="background-color:#000033;">
+<div class="container">
+<div class="navbar-header">
+<button class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" style="color: #cc33ff; font-size:30px;">Sparks Bank</a>
+</div>
+<div class="collapse navbar-collapse" id="mynavbar">
+<ul class="nav navbar-nav navbar-right" style="font-size:20px;">
+<li><a href = "index.php" style="color: #cc33ff;"><span class = "glyphicon glyphicon-home"></span> Home</a></li>
+<li><a href="transaction.php" style="color: #cc33ff;"><span class="glyphicon glyphicon-transfer"></span> Transfer Money</a></li>
+<li><a href="history.php" style="color: #cc33ff;"><span class="glyphicon glyphicon-inbox"></span> History</a></li>
+</ul>
+</div>
+</div>
+</nav>
